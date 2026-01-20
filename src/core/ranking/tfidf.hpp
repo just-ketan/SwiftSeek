ddf7b71ffeb716ed30fe13cc33e1abadb081749d
Ketan <ketanbammidi@gmail.com>
2026-01-20 04:18:04 +0000

    TF-IDF ranker header code

---
+++ src/core/ranking/tfidf.hpp
@@ -0,0 +1,16 @@
+// TF-IDF Scoring interface
+// gives score per document
+
+#pragma once
+#include <vector>
+#include <unordered_map>
+#include <cmath>
+#include "../inverted_index/inverted_index.hpp"
+#include "../document/document_store.hpp"
+
+class TFIDRanker{
+    public:
+        using ScoreDocument = std::pair<DocumentStore::DocId, double>;
+        static std::Vector<ScoreDocument> rank(const std::vector<std::string>& queryTerms, const InvertedIndex& index, std::size_t totalDocuments);
+        // only the definition
+}
