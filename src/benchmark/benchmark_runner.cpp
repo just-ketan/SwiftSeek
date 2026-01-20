80bc74556ff7b1d9fe7b0aa6fc69b2fff7cc3a99
Ketan <ketanbammidi@gmail.com>
2026-01-20 07:22:30 +0000

    benchmarking code for our systems

---
+++ src/benchmark/benchmark_runner.cpp
@@ -0,0 +1,20 @@
+#include "benchmark_runner.hpp"
+#include "../core/query/query_parse.hpp"
+#include "../core/query/query_executor.hpp"
+#include "../core/inverted_index/inverted_index.hpp"
+#include "../utils/logger.hpp"
+#include <chrono>
+
+void BenchmarkRunner::runSearchBenchmark(const std::vector<std::string>& queries){
+    using clock = std::chrono::high_resolution_clock;
+    InvertedIndex index;
+    auto start = clock::now();
+    for(const auto& q:queries){
+        auto parsed = QueryParser::parse(q);
+        QueryExecutor::execute(parsed, index);
+    }
+    auto end = clock.now();
+    auto ms = std::chrono::duration_cast<std::chrono::millisecond>(end-start);
+
+    log(LogLevel::INFO, "Benchmark completed in " + std::to_string(ms.count()) + "ms");
+}
---
+++ src/benchmark/benchmark_runner.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+#include <string>
+
+class BenchmarkRunner{
+    public:
+        static void runSearchBenchmark(const std::vector<std::string>& queries);
+}
