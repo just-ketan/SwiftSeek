bda942c6f5eacf6685c5c0cef9c8a59150dd1b58
Ketan <ketanbammidi@gmail.com>
2026-01-22 12:14:56 +0000

    aho corasick matcher code

---
+++ src/core/automata/aho_matcher.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "aho_corasick.hpp"
+#include "../document/document_store.hpp"
+
+class AhoMatcher{
+    public:
+        explicit AhoMatcher(const std::vector<std::string>& patterns) : automaton_(patterns) {}
+        //check if any patterns appear in the document
+        bool matches(DocumentStore::DocId docId, const DocumentStore& store) const {
+            auto test = store.getDocument(docId);
+            return !automaton_.match(std::string(text)).empty();
+        }
+    private:
+        AhoCorasick automaton_;
+};
