2a2029128bd8c7ce9e51273b4e16d7ebde3760f3
Ketan <ketanbammidi@gmail.com>
2026-01-22 11:57:11 +0000

    aho corasick core logic

---
+++ src/core/automata/aho_corasick.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+class AhoCorasick{
+    public:
+        explicit AhoCorasick(const std::vector<std::string>& patterns);
+        std::vector<int> match(const std::string& text) const;  // find all pattern matches in given string
+    private:
+        struct Node{
+            std::unordered_map<char, int> next;
+            int fail = -1;
+            std::vector<int> output;
+        };
+
+        std::vector<Node> trie_;
+
+        void buildTrie(const std::vector<std::string>& patterns);
+        void buildFailureLinks();
+}
